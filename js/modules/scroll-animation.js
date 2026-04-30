1739ac68f9c7ccdff56683a0b45b87f6094a5feb
marianyarj <marianyarj@gmail.com>
2026-04-30 12:33:19 +0200

    feat: add debounce function to scroll animation

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -1,9 +1,11 @@
+import debounce from "./debounce.js";
+
 export default class ScrollAnimation {
 
     constructor(sections) {
         this.sections = document.querySelectorAll(sections);
         this.windowHalf = window.innerHeight * 0.5;
-        this.checkDistance = this.checkDistance.bind(this);
+        this.checkDistance = debounce(this.checkDistance.bind(this), 100);
     }
 
     getDistance() {
